143fb0f2a170e9ea9cfc41b0412f3c8aecb3a1bf
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-06-28 09:30:23 -0300

    hotfix: fix build

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { FeatureModule } from './feature/feature.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true }), FeatureModule],
+  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
 })
 export class AppModule {}
